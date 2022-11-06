1118908c6200b0db5576c733c9a0d577c3834209
Senay Gebrat <senaygebrat@gmail.com>
2022-11-06 13:59:32 -0500

    fixed server to begin running

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
+// const apiRoutes = require('./api');
+// const homeRoutes = require('./home-routes.js');
+const userRoutes = require('./user-routes.js');
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+// router.use('/', homeRoutes);
+router.use('/users', userRoutes);
+// router.use('/api', apiRoutes);
 
 module.exports = router;
